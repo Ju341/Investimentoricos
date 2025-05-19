42372abe501c130b4f47ee6cff1a0992367da462
Ju341 <211224775+Ju341@users.noreply.github.com>
2025-05-19 21:46:21 +0200

    Pred

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA8CkFxpyRR9S_26GcuIfpPizZgTo6C_es",
+  authDomain: "predictorv4-86be5.firebaseapp.com",
+  databaseURL: "https://predictorv4-86be5-default-rtdb.firebaseio.com",
+  projectId: "predictorv4-86be5",
+  storageBucket: "predictorv4-86be5.appspot.com",
+  messagingSenderId: "205359112886",
+  appId: "1:205359112886:web:5ce22c97594c1ca0137c17"
+};
+
+firebase.initializeApp(firebaseConfig);
